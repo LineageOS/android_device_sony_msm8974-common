f9dac279e2fb218768bb87fae4c13f3e1813581e
Bruno Martins <bgcngm@gmail.com>
2017-11-16 22:40:39 +0000

    msm8974-common: Switch to binderized DRM HAL
    Change-Id: I4999809c288c84d6e9c39383ff76f9ee85a97cae

--- msm8974.mk
+++ msm8974.mk
@@ -93,7 +93,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Dumpstate
 PRODUCT_PACKAGES += \
