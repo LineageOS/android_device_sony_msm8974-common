552a637c84176d171a7a91b602eee7fb281ec938
Daniel Jarai <jaraidaniel@gmail.com>
2019-01-15 16:09:37 +0100

    msm8974-common: Make the external camera provider ignore internal cameras
     * The external camera provider occupies our camera v4l2 nodes,
       potentially crashing the kernel driver and blocking the camera HAL.
       Unfortunately, there's no easy way to just disable it, so let's just
       tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>

--- msm8974.mk
+++ msm8974.mk
@@ -69,6 +69,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
