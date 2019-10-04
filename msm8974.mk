c1f5561b3fe7400660887a9320ba6071812cf161
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    msm8974-common: Switch to commonized legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- msm8974.mk
+++ msm8974.mk
@@ -223,7 +223,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service.legacy
 
 PRODUCT_PACKAGES += \
     libwpa_client \
