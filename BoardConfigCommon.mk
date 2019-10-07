AICP/device_sony_msm8974-common

msm8974-common: Actually apply config.fs
Change-Id: Ifb962d740311853860521453e3caeabc9b0573bb

BoardConfigCommon.mk
@@ -99,6 +99,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 TARGET_NO_RPC := true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN += $(COMMON_PATH)/config.fs
+
 # Graphics
 USE_OPENGL_RENDERER := true
 TARGET_USES_ION := true

LineageOS/android_device_sony_msm8974-common

BoardConfigCommon.mk
@@ -66,6 +66,9 @@ TARGET_LD_SHIM_LIBS := \
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN += $(PLATFORM_PATH)/config.fs
+
 # Graphics
 TARGET_USES_ION := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3