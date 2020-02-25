af85d6ca3d5daf38f7c3a01745d2ec8ffae1f5ad
Arian <arian.kulmer@web.de>
2020-02-25 17:21:02 +0100

    msm8974-common: extract-files: Fixup kang option and minor style changes
    Change-Id: If43e06b445c45630af1a25c435947f1e5374645d

--- extract-files.sh
+++ extract-files.sh
@@ -21,7 +21,7 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+LINEAGE_ROOT="${MY_DIR}"/../../..
 
 HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
@@ -35,37 +35,42 @@ CLEAN_VENDOR=true
 SECTION=
 KANG=
 
-while [ "$1" != "" ]; do
-    case "$1" in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-                                ;;
-        -k | --kang)            KANG="--kang"
-                                ;;
-        -s | --section )        shift
-                                SECTION="$1"
-                                CLEAN_VENDOR=false
-                                ;;
-        * )                     SRC="$1"
-                                ;;
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
 
 if [ -z "${SRC}" ]; then
-    SRC=adb
+    SRC="adb"
 fi
 
 # Initialize the helper for platform
-setup_vendor "${DEVICE_PLATFORM}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE_PLATFORM}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
+        "${KANG}" --section "${SECTION}"
 
-if [ -s "$MY_DIR"/../$DEVICE_COMMON/proprietary-files.txt ]; then
+if [ -s "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" ]; then
     # Reinitialize the helper for common
     source "${MY_DIR}/../${DEVICE_COMMON}/extract-files.sh"
     setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}"/../${DEVICE_COMMON}/proprietary-files.txt "${SRC}" "${SECTION}"
+    extract "${MY_DIR}"/../${DEVICE_COMMON}/proprietary-files.txt "${SRC}" \
+            "${KANG}" --section "${SECTION}"
 fi
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
@@ -73,7 +78,8 @@ if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
     setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}"/../${DEVICE}/proprietary-files.txt "${SRC}" "${SECTION}"
+    extract "${MY_DIR}"/../${DEVICE}/proprietary-files.txt "${SRC}" \
+            "${KANG}" --section "${SECTION}"
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
