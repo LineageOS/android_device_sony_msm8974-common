2f9f10b5e8484a31901db9f5fa3d34eca9afc469
115ek <e.blosz@hotmail.de>
2020-12-04 21:12:46 +0100

    msm8974-common: Move files from /sbin to /bin
    * Android 11 globally removed /sbin
      https://github.com/LineageOS/android_system_core/commit/2f74a26a2dbe092d63f1e46e6471369d4a19adce#diff-ac3a2ef33fb4b89653911e274441a0a00da7c30371c1a36af05f22d054ea4cce
    
    Change-Id: I148cf3ee7e3bef809eb4285ad9b3d9de4189d8fb

--- init/Android.mk
+++ init/Android.mk
@@ -6,18 +6,18 @@ ramdisk_init      := $(TARGET_RAMDISK_OUT)/init
 ramdisk_init_real := $(TARGET_RAMDISK_OUT)/init.real
 
 # If /init is a file and not a symlink then rename it to /init.real
-# and make /init be a symlink to /sbin/init_sony (which will execute
+# and make /init be a symlink to /bin/init_sony (which will execute
 # /init.real, if appropriate.
 $(ramdisk_init_real): $(ramdisk_init) $(TARGET_RECOVERY_ROOT_OUT)/system/bin/toybox_static $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/utilities/init_sony
-	$(hide) if [ ! -d $(TARGET_RAMDISK_OUT)/sbin ]; then \
-		mkdir -p $(TARGET_RAMDISK_OUT)/sbin; \
+	$(hide) if [ ! -d $(TARGET_RAMDISK_OUT)/bin ]; then \
+		mkdir -p $(TARGET_RAMDISK_OUT)/bin; \
 	fi
-	cp $(TARGET_RECOVERY_ROOT_OUT)/system/bin/toybox_static $(TARGET_RAMDISK_OUT)/sbin/toybox_init
-	cp $(PRODUCT_OUT)/utilities/keycheck $(TARGET_RAMDISK_OUT)/sbin/keycheck
-	cp $(PRODUCT_OUT)/utilities/init_sony $(TARGET_RAMDISK_OUT)/sbin/init_sony
+	cp $(TARGET_RECOVERY_ROOT_OUT)/system/bin/toybox_static $(TARGET_RAMDISK_OUT)/bin/toybox_init
+	cp $(PRODUCT_OUT)/utilities/keycheck $(TARGET_RAMDISK_OUT)/bin/keycheck
+	cp $(PRODUCT_OUT)/utilities/init_sony $(TARGET_RAMDISK_OUT)/bin/init_sony
 	$(hide) if [ ! -L $(ramdisk_init) ]; then \
 	  mv $(ramdisk_init) $(ramdisk_init_real); \
-	  ln -s /sbin/init_sony $(ramdisk_init); \
+	  ln -s /bin/init_sony $(ramdisk_init); \
 	else \
 	  echo "/init $(ramdisk_init) is already a symlink"; \
 	fi
